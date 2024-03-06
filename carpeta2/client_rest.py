e11b418c70f662577e65a33cc6008a3de325129b
alopapitas1 <161209593+alopapitas1@users.noreply.github.com>
2024-03-06 10:37:30 -0400

    Update client_rest.py
    cliente rest

--- carpeta2/client_rest.py
+++ carpeta2/client_rest.py
@@ -1,64 +1,46 @@
 import requests
 
-
 url = "http://localhost:8000/"
 
 ruta_get = url + "estudiantes"
+print("ENTRO AL GET")
 get_response = requests.request(method="GET", url=ruta_get)
 print(get_response.text)
 
-ruta_post = url + "estudiantes"
-nuevo_estudiante = {
-    "nombre": "Juanito",
-    "apellido": "Pérez",
-    "carrera": "Ingeniería Agronomica",
-}
 
-post_response = requests.request(method="POST", url=ruta_post, json=nuevo_estudiante)
-print(post_response.text)
-
-
-ruta_eliminar = url + "estudiantes"
-eliminar_response = requests.request(method="DELETE", 
-                                    url=ruta_eliminar)
-print(eliminar_response.text)
+# ruta_post = url + "estudiantes"
+# print("ENTRO AL POST")
+# nuevo_estudiante ={
+#         "id": 5,
+#         "nombre": "Pedrito",
+#         "apellido": "García",
+#         "carrera": "Ingenieria de Sistemas",
+#     }
+# post_response = requests.request(method="POST", url=ruta_post, json=nuevo_estudiante)
+# print(post_response.text)
 
 
-ruta_post = url + "estudiantes"
-nuevo_estudiante = {
-    "nombre": "Juanito",
-    "apellido": "Pérez",
-    "carrera": "Ingeniería Agronomica"
-}
+# ruta_filtrar_nombre = url + "estudiantes/1"
+# print("ENTRO AL GET con id")
+# filtrar_nombre_response = requests.request(method="GET", url=ruta_filtrar_nombre)
+# print(filtrar_nombre_response.text)
 
-post_response = requests.request(method="POST", 
-                        url=ruta_post,
-                        json=nuevo_estudiante)
-print(post_response.text)
 
-nuevo_estudiante = {
-    "nombre": "Pedrito",
-    "apellido": "Lopez",
-    "carrera": "Ingeniería",
-}
 
-post_response = requests.request(method="POST", url=ruta_post, json=nuevo_estudiante)
-print(post_response.text)
+# ruta_actualizar = url + "estudiantes"
+# print("ENTRO AL PUT")
+# estudiante_actualizado = {
+#     "id": 1,
+#     "nombre": "Juan",
+#     "apellido": "Pérez",
+#     "carrera": "Ingeniería Agronomica",
+# }
+# put_response = requests.request(method="PUT", url=ruta_actualizar, json=estudiante_actualizado)
+# print(put_response.text)
 
-ruta_filtrar_nombre = url + "estudiantes/1"
-filtrar_nombre_response = requests.request(method="GET", 
-                                url=ruta_filtrar_nombre)
-print(filtrar_nombre_response.text)
 
-ruta_actualizar = url + "estudiantes"
-estudiante_actualizado = {
-    "id": 1,
-    "nombre": "Juan",
-    "apellido": "Pérez",
-    "carrera": "Ingeniería Agronomica",
-}
-put_response = requests.request(
-    method="PUT", url=ruta_actualizar, 
-    json=estudiante_actualizado
-)
-print(put_response.text)
+
+# ruta_eliminar=url+"estudiantes"
+# print("estudiantes eliminados")
+# delete_response=requests.request(method="DELETE", url=ruta_eliminar)
+# print(delete_response.text)
